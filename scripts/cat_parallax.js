39c111d4d26b742f7588afea79b40b4ba0ebbeec
Jabari Bell <jabari.bell@pxlflu.net>
2012-11-13 13:39:02 -0500

    cleaned up some typos, ugly but the pngs are moving at different speeedsgit add .

--- scripts/cat_parallax.js
+++ scripts/cat_parallax.js
@@ -10,37 +10,37 @@ $(document).ready(function() {
 	    $lolcats_text_3 = $('#lolcats_text_3'),
 	    $lolcats_text_4 = $('#lolcats_text_4');
 	
+	var $lolcats_text_holder = $('#lolcats_text_holder');
+	
+	var windowHeight = $window.height();
+
 	//add inview class to stuff that's in the viewport
-	$('#lolcats_text_1, #lolcats_text_2, #lolcats_text_3, #lolcats_text_4'), function(event, visible) {
+	$('#lolcats_text_holder').bind('inview', function(event, visible) {
 			if(visible == true) {
 				$(this).addClass("inview");
 			} else {
 				$(this).removeClass("inview");
 			}
+			console.log("text holder is: " + visible);
 
 	});
 	
 	//move the div to its new position
-	function newPositon(x, windowHeight, pos, adjuster, inertia) {
-		return x + "% " + (-((windowHeight + pos) - adjuster * inertia) + "px";
+	function newPosition(x, windowHeight, pos, adjuster, inertia) {
+		return x + "% " + (-((windowHeight + pos) - adjuster) * inertia) + "px";
 	}
 	
 	//called whenever window is scrolled
 	function move(){
 		var pos = $window.scrollTop();
-
-		//letters for lolcats
-		if($lolcats_text_1.hasClass("inview")) {
-			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.3)});
-		}
-		if($lolcats_text_2.hasClass("inview")) {
-			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
-		}
-		if($lolcats_text_3.hasClass("inview")) {
-			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.1)});
-		}
-		if($lolcats_text_4.hasClass("inview")) {
-			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.5)});
+		console.log('move being called!');
+		//lolcats intro text
+		if($lolcats_text_holder.hasClass("inview")) {
+			console.log("should be calling newPosition");
+			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 39, 0.3)});
+			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 9, 0.2)});
+			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 92, 0.1)});
+			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 22, 0.5)});
 		}
 	}
 				
