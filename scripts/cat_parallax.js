8cacb103483833bd1471df372b6b0b91fd24d2f1
Jabari Bell <jabari.bell@pxlflu.net>
2012-11-13 12:24:26 -0500

    more cleaning up to cat_parallax.js and the index

--- scripts/cat_parallax.js
+++ scripts/cat_parallax.js
@@ -5,16 +5,13 @@ $(document).ready(function() {
 	var $window = $(window);
 
 	//letters for lolcats
-	var $lolcats_first_l = $('#lolcats_first_l'),
-	    $lolcats_first_o = $('#lolcats_first_o'),
-	    $lolcats_second_l = $'#lolcats_second_l'),
-	    $lolcats_first_c = $('#lolcats_first_c'),
-	    $lolcats_first_a = $('#lolcats_first_a'),
-	    $lolcats_first_t = $('#lolcats_first_t'),
-	    $lolcats_first_s = $('#lolcats_first_s');
-
+	var $lolcats_text_1 = $('#lolcats_text_1'),
+	    $lolcats_text_2 = $('#lolcats_text_2'),
+	    $lolcats_text_3 = $('#lolcats_text_3'),
+	    $lolcats_text_4 = $('#lolcats_text_4');
+	
 	//add inview class to stuff that's in the viewport
-	$('#lolcats_first_l, #lolcats_first_o, #lolcats_second_l, #lolcats_first_c, #lolcats_first_a, #lolcats_first_t, #lolcats_first_s'), function(event, visible) {
+	$('#lolcats_text_1, #lolcats_text_2, #lolcats_text_3, #lolcats_text_4'), function(event, visible) {
 			if(visible == true) {
 				$(this).addClass("inview");
 			} else {
@@ -33,26 +30,17 @@ $(document).ready(function() {
 		var pos = $window.scrollTop();
 
 		//letters for lolcats
-		if($lolcats_first_l.hasClass("inview")) {
-			$lolcats_first_l.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
+		if($lolcats_text_1.hasClass("inview")) {
+			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
 		}
-		if($lolcats_first_o.hasClass("inview")) {
-			$lolcats_first_o.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
+		if($lolcats_text_2.hasClass("inview")) {
+			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
 		}
-		if($lolcats_second_l.hasClass("inview")) {
-			$lolcats_second_l.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
+		if($lolcats_text_3.hasClass("inview")) {
+			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
 		}
-		if($lolcats_first_c.hasClass("inview")) {
-			$lolcats_first_c.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
-		}
-		if($lolcats_first_a.hasClass("inview")) {
-			$lolcats_first_a.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
-		}
-		if($lolcats_first_t.hasClass("inview")) {
-			$lolcats_first_t.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
-		}
-		if($lolcats_first_s.hasClass("inview")) {
-			$lolcats_first_s.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
+		if($lolcats_text_4.hasClass("inview")) {
+			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, 0.2)});
 		}
 	}
 				
