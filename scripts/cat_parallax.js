032f28b64071e2a4dc87a78d7c434cd10e1a7a87
Jabari Bell <jabari.bell@pxlflu.net>
2012-11-13 17:05:48 -0500

    more fudging around, not fixed yet, but ANOTHER COOL INTRO TEXT EFFECT... now just to understand why it works...

--- scripts/cat_parallax.js
+++ scripts/cat_parallax.js
@@ -8,10 +8,32 @@ $(document).ready(function() {
 	var $lolcats_text_1 = $('#lolcats_text_1'),
 	    $lolcats_text_2 = $('#lolcats_text_2'),
 	    $lolcats_text_3 = $('#lolcats_text_3'),
-	    $lolcats_text_4 = $('#lolcats_text_4');
+	    $lolcats_text_4 = $('#lolcats_text_4'),
+	    speedDeltaLowerBound = -0.018,
+	    speedDeltaUpperBound = -0.013,
+	    base_lolcats_text_speed = 0.3,
+	    lolcats_text_adjuster = 500;
+	    round_to = 10000;
+
+	
+	var speedDelta1 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
+	var speedDelta2 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
+	var speedDelta3 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
+	var speedDelta4 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
+
+	//round deltas
+	speedDelta1 = Math.round(round_to*speedDelta1) / round_to;
+	speedDelta2 = Math.round(round_to*speedDelta2) / round_to;
+	speedDelta3 = Math.round(round_to*speedDelta3) / round_to;
+	speedDelta4 = Math.round(round_to*speedDelta4) / round_to;
+
+	console.log('speed delta 1: ' + speedDelta1);
+	console.log('speed delta 2: ' + speedDelta2);
+	console.log('speed delta 3: ' + speedDelta3);
+	console.log('speed delta 4: ' + speedDelta4);
 	
 	var $lolcats_text_holder = $('#lolcats_text_holder');
-	
+
 	var windowHeight = $window.height();
 
 	//add inview class to stuff that's in the viewport
@@ -28,23 +50,32 @@ $(document).ready(function() {
 	//move the div to its new position
 	function newPosition(x, windowHeight, pos, adjuster, inertia) {
 		var returnValue = x + "% " + (-((windowHeight + pos) - adjuster) * inertia) + "px";
-		console.log('return value: ' + returnValue);
+		// console.log('return value: ' + returnValue);
+		// console.log('inertia: ' + inertia);
 		return returnValue;
 	}
 	
 	//called whenever window is scrolled
 	function move(){
-		var windowHeight = $window.height();
+		// var windowHeight = $window.height();
 		var pos = $window.scrollTop();
 		console.log('scroll top is: ' + pos);
 		//lolcats intro text
 		if($lolcats_text_holder.hasClass("inview")) {
-			console.log("should be calling newPosition");
-			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.42)});
-			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.38)});
-			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.33)});
-			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.31)});
+			// console.log("should be calling newPosition");
+			// $lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 900, ((base_lolcats_text_speed + (pos * speedDelta1)) > (8 * (base_lolcats_text_speed + (pos * speedDelta1)))) ? base_lolcats_text_speed : (base_lolcats_text_speed + (pos * speedDelta1)))});
+			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta1)))});
+			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta2)))});
+			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta3)))});
+			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta4)))});
 		}
+	}
+
+	/**
+	 * Returns a random number between min and max
+	 */
+	function getRandomArbitrary (min, max) {
+    		return Math.random() * (max - min) + min;
 	}
 				
 	$window.resize(function(){
