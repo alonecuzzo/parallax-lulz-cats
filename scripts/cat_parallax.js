60077870765ad2dd713356476ec6cadbe3c45bf6
Jabari Bell <jabari.bell@pxlflu.net>
2012-11-13 14:34:56 -0500

    more progress on scrolling, figured out that the .lolcats_text div was cutting off bottom of text

--- scripts/cat_parallax.js
+++ scripts/cat_parallax.js
@@ -27,20 +27,23 @@ $(document).ready(function() {
 	
 	//move the div to its new position
 	function newPosition(x, windowHeight, pos, adjuster, inertia) {
-		return x + "% " + (-((windowHeight + pos) - adjuster) * inertia) + "px";
+		var returnValue = x + "% " + (-((windowHeight + pos) - adjuster) * inertia) + "px";
+		console.log('return value: ' + returnValue);
+		return returnValue;
 	}
 	
 	//called whenever window is scrolled
 	function move(){
+		var windowHeight = $window.height();
 		var pos = $window.scrollTop();
-		console.log('move being called!');
+		console.log('scroll top is: ' + pos);
 		//lolcats intro text
 		if($lolcats_text_holder.hasClass("inview")) {
 			console.log("should be calling newPosition");
-			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 39, 0.3)});
-			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 9, 0.2)});
-			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 92, 0.1)});
-			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 22, 0.5)});
+			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.42)});
+			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.38)});
+			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.33)});
+			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, 500, 0.31)});
 		}
 	}
 				
