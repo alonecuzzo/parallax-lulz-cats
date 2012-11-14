be574c88fc894a60cfc3f50ea96cadb0a24ea656
Jabari Bell <jabari.bell@pxlflu.net>
2012-11-14 17:59:18 -0500

    liking effect, not sure what math is getting it there, but seems to be an oscillator, need to give it limits or it will go infinitely

--- scripts/cat_parallax.js
+++ scripts/cat_parallax.js
@@ -7,12 +7,12 @@ var init = function() {
 	    $lolcats_text_2 = $('#lolcats_text_2'),
 	    $lolcats_text_3 = $('#lolcats_text_3'),
 	    $lolcats_text_4 = $('#lolcats_text_4'),
-	    speedDeltaLowerBound = -0.008,
-	    speedDeltaUpperBound = -0.003,
+	    speedDeltaLowerBound = 0.008,
+	    speedDeltaUpperBound = 0.003,
 	    // to adjust height of intro text, and point of disappearing,
 	    // play with the text speed, the adjuster, and the containing 
 	    // div class and id heights!!!!
-	    base_lolcats_text_speed = 0.3,
+	    base_lolcats_text_speed = 0.3;
 	    lolcats_text_adjuster = 1000,
 	    round_to = 10000;
 	    
@@ -29,26 +29,27 @@ var init = function() {
 	speedDelta3 = Math.round(round_to*speedDelta3) / round_to;
 	speedDelta4 = Math.round(round_to*speedDelta4) / round_to;
 	
-	var $lolcats_text_holder = $('#lolcats_text_holder'),
-	    $lolcats_text = $('.lolcats_text');
-
 	//add inview class to stuff that's in the viewport
-	$('#lolcats_text_holder').bind('inview', function(event, visible) {
+	$('.lolcats_text_1').bind('inview', function(event, visible) {
 			if(visible == true) {
 				$(this).addClass("inview");
 			} else {
 				$(this).removeClass("inview");
 			}
-			console.log("text holder is: " + visible);
+			console.log("text section is: " + visible);
 
 	});
 	
 	//move the div to its new position
 	function newPosition(x, windowHeight, pos, adjuster, inertia, div) {
 		var adjuster1 = 400 + (windowHeight);
-		console.log('adjuster position: ' + adjuster1);
+		adjuster1 = 0;
+		// console.log('adjuster position: ' + adjuster1);
 		var newY = -((windowHeight - pos) - adjuster1) * inertia;
-		var returnValue = x + "% " + newY + "px";
+		newY = Math.abs(newY);
+		// console.log('inertia ' + inertia);
+		// var returnValue = x + "% " + newY + "px";
+		var returnValue = newY + "px";
 		// console.log('return value: ' + returnValue);
 		return returnValue;
 	}
@@ -58,15 +59,20 @@ var init = function() {
 		var windowHeight = $window.height();
 		$window.scrollLeft(0);
 		var pos = $window.scrollTop();
-		// console.log('scroll top is: ' + pos);
+		 // console.log('scroll top is: ' + pos);
 		//lolcats intro text
-		if($lolcats_text_holder.hasClass("inview")) {
+		// if($('.lolcats_text_section').hasClass("inview")) {
 			// console.log("should be calling newPosition");
-			$lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta1)), $lolcats_text_1)});
-			$lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta2)), $lolcats_text_2)});
-			$lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta3)), $lolcats_text_3)});
-			$lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta4)), $lolcats_text_4)});
-		}
+			// $lolcats_text_1.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta1)), $lolcats_text_1)});
+			// $lolcats_text_2.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta2)), $lolcats_text_2)});
+			// $lolcats_text_3.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta3)), $lolcats_text_3)});
+			// $lolcats_text_4.css({'backgroundPosition' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta4)), $lolcats_text_4)});
+			$lolcats_text_1.css({'margin-top' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta1)), $lolcats_text_1)});
+			$lolcats_text_2.css({'margin-top' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta2)), $lolcats_text_2)});
+			$lolcats_text_3.css({'margin-top' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta3)), $lolcats_text_3)});
+			$lolcats_text_4.css({'margin-top' : newPosition(50, windowHeight, pos, lolcats_text_adjuster, (base_lolcats_text_speed + (pos * speedDelta4)), $lolcats_text_4)});
+
+		// }
 	}
 
 	/**
@@ -89,7 +95,7 @@ var init = function() {
 
 $(document).ready(function() {
 	// here until we figure out how to get height adjustment set
-	// init();	
+	 init();	
 	// var lockScrollTop = function() {
 	// 	$(window).scrollTop(0);
 	// };
