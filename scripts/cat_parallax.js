0f3e19d41df1731d274c7d49445101963a3d1a5e
Jabari Bell <jabari.bell@pxlflu.net>
2012-11-14 17:12:54 -0500

    ripping everything down, using imgs instead of the background image property... got lolcats text centering vertically and horizontally

--- scripts/cat_parallax.js
+++ scripts/cat_parallax.js
@@ -12,14 +12,12 @@ var init = function() {
 	    // to adjust height of intro text, and point of disappearing,
 	    // play with the text speed, the adjuster, and the containing 
 	    // div class and id heights!!!!
-	    base_lolcats_text_speed = 0.7,
+	    base_lolcats_text_speed = 0.3,
 	    lolcats_text_adjuster = 1000,
-	    prevY1, prevY2, prevY3, prevY4 = -100000,
 	    round_to = 10000;
 	    
-	    console.log('adjuster: ' + lolcats_text_adjuster);
-
-	
+	    // console.log('adjuster: ' + lolcats_text_adjuster);
+		
 	var speedDelta1 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
 	var speedDelta2 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
 	var speedDelta3 = getRandomArbitrary(speedDeltaLowerBound, speedDeltaUpperBound);	
@@ -30,19 +28,9 @@ var init = function() {
 	speedDelta2 = Math.round(round_to*speedDelta2) / round_to;
 	speedDelta3 = Math.round(round_to*speedDelta3) / round_to;
 	speedDelta4 = Math.round(round_to*speedDelta4) / round_to;
-
-	// console.log('speed delta 1: ' + speedDelta1);
-	// console.log('speed delta 2: ' + speedDelta2);
-	// console.log('speed delta 3: ' + speedDelta3);
-	// console.log('speed delta 4: ' + speedDelta4);
 	
 	var $lolcats_text_holder = $('#lolcats_text_holder'),
-	    $lolcats_text = $('.lolcats_text'),
-	    lolcats_text_window_height_ratio = windowHeight / $lolcats_text.height(),
-	    lolcats_text_orig_width = $lolcats_text.width();
-
-	var orig_lolcats_text_1_width = $lolcats_text_1.width();
-
+	    $lolcats_text = $('.lolcats_text');
 
 	//add inview class to stuff that's in the viewport
 	$('#lolcats_text_holder').bind('inview', function(event, visible) {
@@ -51,53 +39,25 @@ var init = function() {
 			} else {
 				$(this).removeClass("inview");
 			}
-			// console.log("text holder is: " + visible);
+			console.log("text holder is: " + visible);
 
 	});
 	
 	//move the div to its new position
 	function newPosition(x, windowHeight, pos, adjuster, inertia, div) {
-		var newY = -((windowHeight + pos) - adjuster) * inertia;
-		var scaleBy = Math.round(((280 + windowHeight - newY) / windowHeight) * 100) / 100;
-		if(scaleBy > 1){
-			scaleBy = 1;
-		}
-		if(scaleBy < 0) {
-			scaleBy = 0;
-		}
+		var adjuster1 = 400 + (windowHeight);
+		console.log('adjuster position: ' + adjuster1);
+		var newY = -((windowHeight - pos) - adjuster1) * inertia;
 		var returnValue = x + "% " + newY + "px";
-		switch(div.attr("id")){
-			case 'lolcats_text_1':
-				if(prevY1 > -100000){
-					// if(prevY1 < newY){
-						console.log('we are headed down: ' + scaleBy);
-						// div.width(scaleBy * orig_lolcats_text_1_width);
-						// $lolcats_text.transform({'scale' : [scaleBy, scaleBy], 'origin' : ['35%', '90%']});
-						$('#lolcats_text_1').transform({'scale' : [scaleBy, scaleBy], 'origin' : ['35%', '90%']});
-						$('#lolcats_text_2').transform({'scale' : [scaleBy, scaleBy], 'origin' : ['35%', '90%']});
-						$('#lolcats_text_3').transform({'scale' : [scaleBy, scaleBy], 'origin' : ['35%', '90%']});
-						$('#lolcats_text_4').transform({'scale' : [scaleBy, scaleBy], 'origin' : ['35%', '90%']});
-						
-						// $lolcats_text.css({opacity : scaleBy - .4});
-
-					// }
-				}
-				prevY1 = newY;
-				break;
-		}
-		// console.log(div.attr("id"));
-		// pY = newY;
 		// console.log('return value: ' + returnValue);
-		// console.log('inertia: ' + inertia);
 		return returnValue;
 	}
 	
 	//called whenever window is scrolled
 	function move(){
-		// var windowHeight = $window.height();
+		var windowHeight = $window.height();
 		$window.scrollLeft(0);
 		var pos = $window.scrollTop();
-		// console.log('text adjuster: ' + lolcats_text_adjuster);
 		// console.log('scroll top is: ' + pos);
 		//lolcats intro text
 		if($lolcats_text_holder.hasClass("inview")) {
@@ -115,33 +75,8 @@ var init = function() {
 	function getRandomArbitrary (min, max) {
     		return Math.random() * (max - min) + min;
 	}
-
-	function repositionVerticalElements(){
-		//need to reposition bottom break to always be x number of pixels shorter than the window height
-		var windowHeight = $(window).height();
-		if(windowHeight > 350){
-			var lolcats_text_window_height_ratio = (windowHeight / $('.lolcats_text').height());
-			var lolcats_text_height = $lolcats_text.height();
-			var new_lolcats_text_height =  ((windowHeight + 900) / lolcats_text_window_height_ratio) * lolcats_text_window_height_ratio;
-			$lolcats_text.height(new_lolcats_text_height);
-			// $lolcats_text.css({'backgroundColor' : 'red'});
-			console.log('new_lolcats_text_height: ' + new_lolcats_text_height);
-			console.log('window height inside vert: ' + windowHeight);
-			console.log('lolcats ratio: ' + lolcats_text_window_height_ratio);
-			var new_lolcats_text_holder_height = $lolcats_text.height() + (lolcats_text_window_height_ratio * 450);
-			lolcats_text_adjuster = ($lolcats_text.height() + 385) * lolcats_text_window_height_ratio;
-	
-			$lolcats_text_holder.css({'height' : new_lolcats_text_holder_height + 'px'});
-			console.log('window height: ' + windowHeight);
-			console.log('');
-		}
-
-		console.log('window height: ' + windowHeight);
-	}
-
 				
 	$window.resize(function(){
-		repositionVerticalElements();
 		move();
 	});
 
@@ -149,21 +84,21 @@ var init = function() {
 		move();
 	});
 
-	repositionVerticalElements();
 	move();
 };
 
 $(document).ready(function() {
-	init();	
-	var lockScrollTop = function() {
-		$(window).scrollTop(0);
-	};
-	
-	// force window not to scroll
-	$(window).bind('scroll', lockScrollTop);	
+	// here until we figure out how to get height adjustment set
+	// init();	
+	// var lockScrollTop = function() {
+	// 	$(window).scrollTop(0);
+	// };
+	// 
+	// // force window not to scroll
+	// $(window).bind('scroll', lockScrollTop);	
 
-	$('#lolcats_text_holder').fadeIn(3000, function(){
-		$(window).unbind('scroll', lockScrollTop);
-		//init();
-	});
+	// $('.lolcats_text').fadeIn(3000, function(){
+	// 	$(window).unbind('scroll', lockScrollTop);
+	// 	//init();
+	// });
 });
